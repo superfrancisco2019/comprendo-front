f1f79b96cb7f9d8b486da9f621a148cb257e0e35
François Boisselier <francois.boisselier@gmail.com>
2020-03-31 14:17:42 +0200

    first page

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="button">
+    <button v-on:click='x'> Créer un nouvel évènement  </button>
+    <p> Rejoindre un évènement </p>
+    <form id="form" @submit="checkform" action="" method="post">
+  </div> 
+</template>
+
+<script>
+export default {
+  name: 'Button'
+}
+</script>
+
+<style scoped>
+  #button{
+  width:500px;
+  height:400px
+  color:black;}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ text }}</h2>   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Comprendo',
+      text: "L'important n'est pas de participer mais de comprendre"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  color:blue;
+}
+</style>
